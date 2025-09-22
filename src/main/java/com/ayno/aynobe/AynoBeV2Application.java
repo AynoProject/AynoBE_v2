6f2eacce784c7b32df894dc11ee9b0d37a753549
yangjunsig <wnstlr1379@naver.com>
2025-09-22 16:03:52 +0900

    first commit

---
+++ src/main/java/com/ayno/aynobe/AynoBeV2Application.java
@@ -0,0 +1,13 @@
+package com.ayno.aynobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AynoBeV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AynoBeV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ayno/aynobe/AynoBeV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayno.aynobe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AynoBeV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
